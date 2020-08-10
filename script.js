c223ee695f34223705159d9620238a7f033f88e3
agkosd <sonawaneakshay039@gmail.com>
2020-08-10 14:52:11 +1000

    code tidy up and image size fix

--- script.js
+++ script.js
@@ -1,65 +1,63 @@
-const TypeWriter = function(txtElement, words, wait=3000){
-	this.txtElement = txtElement;
-	this.words = words;
-	this.txt = '';
-	this.wait = parseInt(wait,10);
-	this.wordIndex = 0;
-	this.type();
-	this.isDeleting = false;
-}
+const TypeWriter = function (txtElement, words, wait = 3000) {
+  this.txtElement = txtElement;
+  this.words = words;
+  this.txt = "";
+  this.wait = parseInt(wait, 10);
+  this.wordIndex = 0;
+  this.type();
+  this.isDeleting = false;
+};
 
-TypeWriter.prototype.type = function(){
-	const current = this.wordIndex % this.words.length;
-	const fullTxt = this.words[current];
+TypeWriter.prototype.type = function () {
+  const current = this.wordIndex % this.words.length;
+  const fullTxt = this.words[current];
 
-	if(this.isDeleting){
-		this.txt = fullTxt.substring(0, this.txt.length -1);
-	}else{
-		this.txt = fullTxt.substring(0, this.txt.length+1);
-	}
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
 
-	this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+  this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
 
-	let typeSpeed = 300;
+  let typeSpeed = 300;
 
-	if(this.isDeleting){
-		typeSpeed /= 2;
-	}
+  if (this.isDeleting) {
+    typeSpeed /= 2;
+  }
 
-	if(!this.isDeleting && this.txt === fullTxt){
-		typeSpeed = this.wait;
-		this.isDeleting = true;
-	}else if(this.isDeleting && this.txt === ''){
-		this.isDeleting = false;
-		this.wordIndex++;
-		typeSpeed = 500;
-	}
-	setTimeout(() => this.type(), typeSpeed);
-}
+  if (!this.isDeleting && this.txt === fullTxt) {
+    typeSpeed = this.wait;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    this.wordIndex++;
+    typeSpeed = 500;
+  }
+  setTimeout(() => this.type(), typeSpeed);
+};
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
-function init(){
-	const txtElement = document.querySelector('.txt-type');
-	const words = JSON.parse(txtElement.getAttribute('data-words'));
-	const wait = txtElement.getAttribute('data-wait');
-	new TypeWriter(txtElement, words, wait);
-
+function init() {
+  const txtElement = document.querySelector(".txt-type");
+  const words = JSON.parse(txtElement.getAttribute("data-words"));
+  const wait = txtElement.getAttribute("data-wait");
+  new TypeWriter(txtElement, words, wait);
 }
 
 // hamburger menu animation
 
-const ham = document.querySelector('.container');
-const bar1 = document.querySelector('.bar1');
-const bar2 = document.querySelector('.bar2');
-const bar3 = document.querySelector('.bar3');
-const ul = document.querySelector('ul');
-ham.addEventListener('click',(e)=>{
-	ham.classList.toggle('show');
-	bar1.classList.toggle('show');
-	bar2.classList.toggle('show');
-	bar3.classList.toggle('show');
-		ul.classList.add('show');
-		ul.classList.toggle('hid');
-
+const ham = document.querySelector(".container");
+const bar1 = document.querySelector(".bar1");
+const bar2 = document.querySelector(".bar2");
+const bar3 = document.querySelector(".bar3");
+const ul = document.querySelector("ul");
+ham.addEventListener("click", (e) => {
+  ham.classList.toggle("show");
+  bar1.classList.toggle("show");
+  bar2.classList.toggle("show");
+  bar3.classList.toggle("show");
+  ul.classList.add("show");
+  ul.classList.toggle("hid");
 });
